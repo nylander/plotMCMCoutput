e835d299129e7af4fc083b34c7fad414f6c16c08
nylander <j.a.a.nylander@gmail.com>
2016-05-31 18:04:40 +0200

    implemented live plotting. Testing remains.

--- plotMCMCoutput.sh
+++ plotMCMCoutput.sh
@@ -9,53 +9,54 @@
 function usage {
 cat <<End_Of_Usage
 
-
 `basename $0` version 05/31/2016
 
 What:
-           Wrapper for plotting MCMC output with gnuplot (gnuplot required)
+          Wrapper for plotting MCMC output with gnuplot (gnuplot required)
 
 By:
-           johan.nylander \@ bils.se
+          johan.nylander \@ bils.se
 
 Usage:
-           `basename $0` [-b burnin] [-o file] [[-c column] | [-x column][-y column]] [-l] file(s)
+          `basename $0` [-b burnin] [-o file | -l, -t] [[-c column] | [-x column][-y column]] file(s)
 
 Options:
-           -b burnin  -- specify the number of generations to be discarded
-           -c column  -- specify column number. First column is nr. 1.
-           -x column  -- use together with -y to plot column y against column x
-           -y column  -- as -c, or use with -x to plot column y against column x
-           -t         -- plot in terminal instead of device
-           -o file    -- plot to file (png format) instead of device
-           -l         -- follow and plot a growing ("live") file
-           -v         -- be verbose
-           -h         -- print help message
+          -b burnin  -- specify the number of generations to be discarded
+          -c column  -- specify column number. First column is nr. 1.
+          -x column  -- use together with -y to plot column y against column x
+          -y column  -- as -c, or use with -x to plot column y against column x
+          -t         -- plot in terminal instead of device
+          -o file    -- plot to file (png format) instead of device
+          -l         -- plot a growing ("live") file to device or terminal
+          -v         -- be verbose
+          -h         -- print help message
 
-Examples:
-           `basename $0` *.p
-           `basename $0` -t *.p
-           `basename $0` -o out.png *.p
-           `basename $0` -b 50000 -c 2 file.p
-           `basename $0` -b 50000 -x 1 -y 2 file
-           `basename $0` -t -l *.p
+Examples: 
+          `basename $0` *.p
+          `basename $0` -t *.p
+          `basename $0` -o out.png *.p
+          `basename $0` -b 50000 -c 2 file.p
+          `basename $0` -b 50000 -x 1 -y 2 file
+          `basename $0` -t -l *.p
 
-Note:
-           Pay attention to gnuplot's ability to use numbering
-           of, e.g., MCMC generations/samples!
-           For example, compare the output from the following
-           commands on an output file (.p) from MrBayes:
-           `basename $0` -b 50 file.p
-           `basename $0` -b 50 -c 2 file.p
-           `basename $0` -b 50 -x 1 -y 2 file.p
+Notes:
+          Pay attention to gnuplot's ability to use numbering
+          of, e.g., MCMC generations/samples!
+          For example, compare the output from the following
+          commands on an output file (.p) from MrBayes:
+          `basename $0` -b 50 file.p
+          `basename $0` -b 50 -c 2 file.p
+          `basename $0` -b 50 -x 1 -y 2 file.p
 
-           The line count done when using the -v option ignore lines starting
-           with # or [, but might include the header line. Gnuplot will ignore
-           any commented lines (starting with # or [), and will handle headers
-           correctly.
+          The line count done when using the -v option ignore lines starting
+          with # or [, but might include the header line. Gnuplot will ignore
+          any commented lines (starting with # or [), and will handle headers
+          correctly.
 
-           The script uses the standard unix/linux/macosx tools 'grep', 'head',
-           'tail, 'awk', and 'wc'. Portability is, however, mostly untested.
+          The script uses the standard unix/linux/macosx tools 'grep', 'head',
+          'tail, 'awk', and 'wc'. Portability is, however, mostly untested.
+
+          If using the '-l' option, you need to abort using Ctrl+C when needed.
 
 End_Of_Usage
     
@@ -88,161 +89,170 @@ IMGFORMAT='png'
 while getopts 'x:y:b:c:o:f:lvht' OPTION
 do
   case $OPTION in
-  b)	  bflag=1
-		  bval="$OPTARG"
-		  ;;
-  c)	  cflag=1
-		  cval="$OPTARG"
-		  ;;
-  x)	  xflag=1
-		  xval="$OPTARG"
-		  ;;
-  y)	  yflag=1
-		  yval="$OPTARG"
-		  ;;
-  o)	  oflag=1
-		  oval="$OPTARG"
-		  ;;
-  f)	  fflag=1
-		  fval="$OPTARG"
-		  ;;
-  l)      lflag=1
-          ;;
-  v)      vflag=1
-          ;;
-  t)      tflag=1
-          ;;
-  h)     usage
-          exit 2
-		  ;;
+  b) bflag=1
+     bval="$OPTARG"
+     ;;
+  c) cflag=1
+     cval="$OPTARG"
+     ;;
+  x) xflag=1
+     xval="$OPTARG"
+     ;;
+  y) yflag=1
+     yval="$OPTARG"
+     ;;
+  o) oflag=1
+     oval="$OPTARG"
+     ;;
+  f) fflag=1
+     fval="$OPTARG"
+     ;;
+  l) lflag=1
+     ;;
+  v) vflag=1
+     ;;
+  t) tflag=1
+     ;;
+  h) usage
+     exit 2
+     ;;
   esac
 done
 shift $(($OPTIND - 1))
 
-
 ## Put remaining args in files, and let gnuplot choke on non existing files or wrong flags...
 FILES="$*"
 if [ "$vflag" ] ; then
-    echo "files to read: $FILES"
-    for file in $FILES ; do
-        linecount=$(grep -v '^#' $file | grep -v '^\[' | wc -l) # not counting commented lines (starting with # or [)
-        echo "  file $file have"
-        echo "    (uncommented) lines: $linecount"
-        echo -n "    columns: "
-        tail -10 $file | head -1 | awk '{ print NF}' # assuming that a few lines from the end is representative
-    done
+  echo "files to read: $FILES"
+  for file in $FILES ; do
+    linecount=$(grep -v '^#' $file | grep -v '^\[' | wc -l) # not counting commented lines (starting with # or [)
+    echo "  file $file have"
+    echo "    (uncommented) lines: $linecount"
+    echo -n "    columns: "
+    tail -10 $file | head -1 | awk '{ print NF}' # assuming that a few lines from the end is representative
+  done
 fi
-
 
 ## Check burnin
 if [ "$bflag" ] ; then
-    if [ "$bval" -ge 0 ] ; then
-        BURNIN=$bval
-      else
-          echo ''
-          echo "arg \"$bval\" is not a number."
-          usage
-          echo ''
-          exit 1
+  if [ "$bval" -ge 0 ] ; then
+    BURNIN=$bval
+  else
+    echo ''
+    echo "arg \"$bval\" is not a number."
+    usage
+    echo ''
+    exit 1
     fi
 else
-    BURNIN=$burnin
+  BURNIN=$burnin
 fi
 
-
-## columns
+## Columns
 if [ "$cflag" ] ; then
-    YCOLUMN=$cval
+  YCOLUMN=$cval
 elif [ "$yflag" ] ; then
-        YCOLUMN=$yval
+  YCOLUMN=$yval
 else
-    YCOLUMN=$ycolumn
+  YCOLUMN=$ycolumn
 fi
 if [ "$xflag" ] ; then
-    XCOLUMN=$xval
+  XCOLUMN=$xval
 else
-    XCOLUMN=$xcolumn
+  XCOLUMN=$xcolumn
 fi
 
-
-## the using argument
+## The using argument
 if [ "$cflag" ] ; then
-    USING="$YCOLUMN"
+  USING="$YCOLUMN"
 elif [ "$yflag" ] ; then
-    if [ $xflag ] ; then
-        USING="$XCOLUMN:$YCOLUMN"
-    else
-        USING="$YCOLUMN"
-    fi
-else
+  if [ $xflag ] ; then
     USING="$XCOLUMN:$YCOLUMN"
+  else
+    USING="$YCOLUMN"
+  fi
+else
+  USING="$XCOLUMN:$YCOLUMN"
 fi
 
-
-## plot in terminal or not
+## Plot in terminal or not
 if [ "$tflag" ] ; then
-    DUMMYTERM="set terminal dumb; "
+  DUMMYTERM="set terminal dumb; "
 fi
 
-
-## plot to file or not
+## Plot to file or not
 if [ "$oflag" ] ; then
-    if [ "$fflag" ]; then
-        IMGFORMAT=$fval
-    fi
-    if [ ! "${oval##*.}" = "$IMGFORMAT" ]; then
-        OUTFILE=${oval}.$IMGFORMAT
-    else
-        OUTFILE=$oval
-    fi
-    FILETERM="set terminal $IMGFORMAT; set output '$OUTFILE';"
+  if [ "$fflag" ]; then
+    IMGFORMAT=$fval
+  fi
+  if [ ! "${oval##*.}" = "$IMGFORMAT" ]; then
+    OUTFILE=${oval}.$IMGFORMAT
+  else
+    OUTFILE=$oval
+  fi
+  FILETERM="set terminal $IMGFORMAT; set output '$OUTFILE';"
 fi
 
-
-## live plot or not
-#if [ "$lflag" ] ; then
-
-    # live plotting can be accomplished by using gnuplot commands
-    # 'pause 1' and 'reread'
-    # crux is to allow it to repeat as long as infile exists,
-    # and also how to dynamically handle changing x and y ranges
-
-#fi
-
+## Live plot or not
+if [ "$lflag" ] ; then
+  TMPFILE=$(mktemp -p "." --suffix=".gnuplot.cmd")
+  trap ctrl_c INT
+  function ctrl_c() {
+    echo "# Trapped CTRL-C. Exiting."
+    if [ -e "$TMPFILE" ] ; then
+      rm -r "$TMPFILE"
+    fi
+    exit 1
+  }
+fi
 
 ## Plot files with gnuplot
 if [ "$vflag" ] ; then
-    echo -n "will try to plot column nr: $YCOLUMN"
-    if [ "$xval" ] ; then
-        echo " against: $XCOLUMN"
-    else
-        echo ''
-    fi
-    echo "burnin set to: $BURNIN."
-    echo 'gnuplot command:'
+  echo -n "will try to plot column nr: $YCOLUMN"
+  if [ "$xval" ] ; then
+    echo " against: $XCOLUMN"
+  else
     echo ''
-    if [ "$oflag" ] ; then
-        echo ''$FILETERM'set datafile commentschars "#[";set key right bottom;plot ['$BURNIN':] [:] for [filename in '\"$FILES\"'] filename using '$USING' with lines title filename'
-    else
-        echo ''$DUMMYTERM'set datafile commentschars "#[";set key right bottom;plot ['$BURNIN':] [:] for [filename in '\"$FILES\"'] filename using '$USING' with lines title filename'
-    fi
-    echo ''
+  fi
+  echo "burnin set to: $BURNIN."
+  echo 'gnuplot command:'
+  echo ''
+  if [ "$oflag" ] ; then
+    echo ''$FILETERM'set datafile commentschars "#[";set key right bottom;plot ['$BURNIN':] [:] for [filename in '\"$FILES\"'] filename using '$USING' with lines title filename'
+  else
+    echo ''$DUMMYTERM'set datafile commentschars "#[";set key right bottom;plot ['$BURNIN':] [:] for [filename in '\"$FILES\"'] filename using '$USING' with lines title filename'
+  fi
+  echo ''
 fi
-
 
 ## Do the actual plotting
 if [ "$FILES" ] ; then 
-    if [ "$oflag" ] ; then
-        echo -e ''$FILETERM'set datafile commentschars "#[";set key right bottom;plot ['$BURNIN':] [:] for [filename in '\"$FILES\"'] filename using '$USING' with lines title filename' | $GNUPLOT
-    else
-        ## Testing live plot 05/31/2016 10:54:55 AM
-        echo -e ''$DUMMYTERM'set datafile commentschars "#[";set key right bottom;plot ['$BURNIN':] [:] for [filename in '\"$FILES\"'] filename using '$USING' with lines title filename; reread' | $GNUPLOT --persist
-    fi
+  if [ "$oflag" ] ; then
+    echo -e ''$FILETERM'set datafile commentschars "#[";set key right bottom;plot ['$BURNIN':] [:] for [filename in '\"$FILES\"'] filename using '$USING' with lines title filename' | $GNUPLOT
+  elif [ "$lflag" ] ; then
+    echo -e ''$DUMMYTERM'set datafile commentschars "#[";set key right bottom;plot ['$BURNIN':] [:] for [filename in '\"$FILES\"'] filename using '$USING' with lines title filename' > "$TMPFILE"
+    echo "pause 1" >> "$TMPFILE"
+    echo "reread" >> "$TMPFILE"
+    echo "(Use Ctrl+C to abort)"
+    "$GNUPLOT" "$TMPFILE"
+  else
+    echo -e ''$DUMMYTERM'set datafile commentschars "#[";set key right bottom;plot ['$BURNIN':] [:] for [filename in '\"$FILES\"'] filename using '$USING' with lines title filename' | $GNUPLOT --persist
+  fi
 else
-    usage
-    echo
-    exit 1
+  usage
+  echo
+  exit 1
 fi
+
+## Remove temp file, if still around
+if [ "$lflag" ] ; then
+  if [ -e "$TMPFILE" ] ; then
+    rm -r "$TMPFILE"
+  fi
+fi
+
+exit 1
+
 
 # if [ "$FILES" ] ; then 
 #     if [ "$oflag" ] ; then
